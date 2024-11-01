e1002bcc069865569b391fb1a309540bb281a2d2
Roky <apple@Rokys-MacBook-Air.local>
2024-11-01 16:08:55 +0900

    project setup

---
+++ tailwind.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          "gradient-start": '#FF8938',
+          "gradient-end": '#F00',
+        }
+      }
+    }
+  }
